a0195185c9d0349da88e59427058eca4a0b96e73
igorkrupitsky <igorkrupitsky@gmail.com>
2025-05-25 00:41:36 -0400

    Initial commit

---
+++ ChatGPT/Form1.Designer.vb
@@ -0,0 +1,251 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmGPTChat
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.txtQuestion = New System.Windows.Forms.TextBox()
+        Me.txtAnswer = New System.Windows.Forms.TextBox()
+        Me.txtUserID = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtTemperature = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtMaxTokens = New System.Windows.Forms.TextBox()
+        Me.cbModel = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.cbVoice = New System.Windows.Forms.ComboBox()
+        Me.lblVoice = New System.Windows.Forms.Label()
+        Me.chkListen = New System.Windows.Forms.CheckBox()
+        Me.chkMute = New System.Windows.Forms.CheckBox()
+        Me.lblSpeech = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSend
+        '
+        Me.btnSend.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnSend.Location = New System.Drawing.Point(3, 410)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(123, 55)
+        Me.btnSend.TabIndex = 0
+        Me.btnSend.Text = "Send"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'txtQuestion
+        '
+        Me.txtQuestion.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtQuestion.Location = New System.Drawing.Point(3, 511)
+        Me.txtQuestion.Multiline = True
+        Me.txtQuestion.Name = "txtQuestion"
+        Me.txtQuestion.Size = New System.Drawing.Size(1177, 130)
+        Me.txtQuestion.TabIndex = 1
+        '
+        'txtAnswer
+        '
+        Me.txtAnswer.AcceptsReturn = True
+        Me.txtAnswer.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtAnswer.Location = New System.Drawing.Point(3, 12)
+        Me.txtAnswer.Multiline = True
+        Me.txtAnswer.Name = "txtAnswer"
+        Me.txtAnswer.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtAnswer.Size = New System.Drawing.Size(1177, 392)
+        Me.txtAnswer.TabIndex = 2
+        '
+        'txtUserID
+        '
+        Me.txtUserID.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.txtUserID.Location = New System.Drawing.Point(229, 430)
+        Me.txtUserID.Name = "txtUserID"
+        Me.txtUserID.Size = New System.Drawing.Size(100, 26)
+        Me.txtUserID.TabIndex = 3
+        Me.txtUserID.Text = "1"
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(159, 433)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "User ID"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(359, 433)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(104, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Randomness"
+        '
+        'txtTemperature
+        '
+        Me.txtTemperature.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.txtTemperature.Location = New System.Drawing.Point(469, 430)
+        Me.txtTemperature.Name = "txtTemperature"
+        Me.txtTemperature.Size = New System.Drawing.Size(100, 26)
+        Me.txtTemperature.TabIndex = 5
+        Me.txtTemperature.Text = "0.5"
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(601, 433)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(90, 20)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Max tokens"
+        '
+        'txtMaxTokens
+        '
+        Me.txtMaxTokens.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.txtMaxTokens.Location = New System.Drawing.Point(697, 430)
+        Me.txtMaxTokens.Name = "txtMaxTokens"
+        Me.txtMaxTokens.Size = New System.Drawing.Size(100, 26)
+        Me.txtMaxTokens.TabIndex = 7
+        Me.txtMaxTokens.Text = "2048"
+        '
+        'cbModel
+        '
+        Me.cbModel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.cbModel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbModel.FormattingEnabled = True
+        Me.cbModel.Items.AddRange(New Object() {"text-davinci-003", "text-davinci-002", "code-davinci-002", "gpt-3.5-turbo", "gpt-3.5-turbo-0301"})
+        Me.cbModel.Location = New System.Drawing.Point(890, 425)
+        Me.cbModel.Name = "cbModel"
+        Me.cbModel.Size = New System.Drawing.Size(291, 28)
+        Me.cbModel.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(832, 430)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(52, 20)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Model"
+        '
+        'cbVoice
+        '
+        Me.cbVoice.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.cbVoice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbVoice.FormattingEnabled = True
+        Me.cbVoice.Location = New System.Drawing.Point(890, 468)
+        Me.cbVoice.Name = "cbVoice"
+        Me.cbVoice.Size = New System.Drawing.Size(291, 28)
+        Me.cbVoice.TabIndex = 11
+        '
+        'lblVoice
+        '
+        Me.lblVoice.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblVoice.AutoSize = True
+        Me.lblVoice.Location = New System.Drawing.Point(832, 471)
+        Me.lblVoice.Name = "lblVoice"
+        Me.lblVoice.Size = New System.Drawing.Size(49, 20)
+        Me.lblVoice.TabIndex = 12
+        Me.lblVoice.Text = "Voice"
+        '
+        'chkListen
+        '
+        Me.chkListen.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.chkListen.AutoSize = True
+        Me.chkListen.Location = New System.Drawing.Point(469, 472)
+        Me.chkListen.Name = "chkListen"
+        Me.chkListen.Size = New System.Drawing.Size(78, 24)
+        Me.chkListen.TabIndex = 13
+        Me.chkListen.Text = "Listen"
+        Me.chkListen.UseVisualStyleBackColor = True
+        '
+        'chkMute
+        '
+        Me.chkMute.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.chkMute.AutoSize = True
+        Me.chkMute.Location = New System.Drawing.Point(697, 471)
+        Me.chkMute.Name = "chkMute"
+        Me.chkMute.Size = New System.Drawing.Size(71, 24)
+        Me.chkMute.TabIndex = 14
+        Me.chkMute.Text = "Mute"
+        Me.chkMute.UseVisualStyleBackColor = True
+        '
+        'lblSpeech
+        '
+        Me.lblSpeech.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblSpeech.AutoSize = True
+        Me.lblSpeech.Location = New System.Drawing.Point(12, 478)
+        Me.lblSpeech.Name = "lblSpeech"
+        Me.lblSpeech.Size = New System.Drawing.Size(73, 20)
+        Me.lblSpeech.TabIndex = 15
+        Me.lblSpeech.Text = "speech..."
+        Me.lblSpeech.Visible = False
+        '
+        'frmGPTChat
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1192, 653)
+        Me.Controls.Add(Me.lblSpeech)
+        Me.Controls.Add(Me.chkMute)
+        Me.Controls.Add(Me.chkListen)
+        Me.Controls.Add(Me.lblVoice)
+        Me.Controls.Add(Me.cbVoice)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.cbModel)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtMaxTokens)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtTemperature)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtUserID)
+        Me.Controls.Add(Me.txtAnswer)
+        Me.Controls.Add(Me.txtQuestion)
+        Me.Controls.Add(Me.btnSend)
+        Me.Name = "frmGPTChat"
+        Me.Text = "GPT Chat"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSend As Button
+    Friend WithEvents txtQuestion As TextBox
+    Friend WithEvents txtAnswer As TextBox
+    Friend WithEvents txtUserID As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtTemperature As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtMaxTokens As TextBox
+    Friend WithEvents cbModel As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents cbVoice As ComboBox
+    Friend WithEvents lblVoice As Label
+    Friend WithEvents chkListen As CheckBox
+    Friend WithEvents chkMute As CheckBox
+    Friend WithEvents lblSpeech As Label
+End Class
---
+++ ChatGPT/Form1.vb
@@ -0,0 +1,288 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Configuration
+Imports System.Security.Cryptography
+Imports System.Speech.Synthesis
+Imports System.Speech.Recognition
+Imports System.Numerics
+
+Public Class frmGPTChat
+
+    Dim OPENAI_API_KEY As String = "" 'https://beta.openai.com/account/api-keys
+    Dim oSpeechRecognitionEngine As SpeechRecognitionEngine = Nothing
+    Dim oSpeechSynthesizer As System.Speech.Synthesis.SpeechSynthesizer = Nothing
+
+    Private Sub frmGPTChat_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim oAppSettingsReader As New AppSettingsReader()
+        Dim sApiKey As String = oAppSettingsReader.GetValue("OPENAI_API_KEY", GetType(String)) & ""
+
+        If sApiKey = "" Then
+            MsgBox("Please enter your OpenAI API key in the App.config file.")
+            End
+        Else
+            OPENAI_API_KEY = sApiKey
+        End If
+
+        'SetModels()
+        cbModel.SelectedIndex = 0
+
+        cbVoice.Items.Clear()
+        Dim synth As New SpeechSynthesizer()
+        For Each voice In synth.GetInstalledVoices()
+            cbVoice.Items.Add(voice.VoiceInfo.Name)
+        Next
+        cbVoice.SelectedIndex = 0
+
+    End Sub
+
+    Private Sub chkListen_CheckedChanged(sender As Object, e As EventArgs) Handles chkListen.CheckedChanged
+        If chkListen.Checked Then
+            lblSpeech.Text = ""
+            lblSpeech.Visible = True
+            SpeechToText()
+        Else
+            oSpeechRecognitionEngine.RecognizeAsyncStop()
+            lblSpeech.Visible = False
+        End If
+    End Sub
+    Private Sub chkMute_CheckedChanged(sender As Object, e As EventArgs) Handles chkMute.CheckedChanged
+
+        If chkMute.Checked Then
+            lblVoice.Visible = False
+            cbVoice.Visible = False
+        Else
+            lblVoice.Visible = True
+            cbVoice.Visible = True
+        End If
+
+    End Sub
+
+    Private Sub SpeechToText()
+
+        If oSpeechRecognitionEngine IsNot Nothing Then
+            oSpeechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple)
+            Exit Sub
+        End If
+
+        oSpeechRecognitionEngine = New SpeechRecognitionEngine(New System.Globalization.CultureInfo("en-US"))
+        oSpeechRecognitionEngine.LoadGrammar(New DictationGrammar())
+        AddHandler oSpeechRecognitionEngine.SpeechRecognized, AddressOf OnSpeechRecognized
+        AddHandler oSpeechRecognitionEngine.SpeechHypothesized, AddressOf OnSpeechHypothesized
+        oSpeechRecognitionEngine.SetInputToDefaultAudioDevice()
+        oSpeechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple)
+    End Sub
+
+    Private Sub OnSpeechRecognized(sender As Object, e As SpeechRecognizedEventArgs)
+        lblSpeech.Text = "" 'Reset Hypothesized text
+
+        If txtQuestion.Text <> "" Then
+            txtQuestion.Text += vbCrLf
+        End If
+
+        Dim text As String = e.Result.Text
+        txtQuestion.Text += text
+    End Sub
+
+    Private Sub OnSpeechHypothesized(sender As Object, e As SpeechHypothesizedEventArgs)
+        Dim text As String = e.Result.Text
+        lblSpeech.Text = text
+    End Sub
+
+    Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
+
+        Dim sQuestion As String = txtQuestion.Text
+        If sQuestion = "" Then
+            MsgBox("Type in your question!")
+            txtQuestion.Focus()
+            Exit Sub
+        End If
+
+        If txtAnswer.Text <> "" Then
+            txtAnswer.AppendText(vbCrLf)
+        End If
+
+        txtAnswer.AppendText("Me: " & sQuestion & vbCrLf)
+        txtQuestion.Text = ""
+
+        Try
+            Dim sAnswer As String = SendMsg(sQuestion)
+            txtAnswer.AppendText("Chat GPT: " & Trim(Replace(sAnswer, vbLf, vbCrLf)))
+            SpeechToText(sAnswer)
+        Catch ex As Exception
+            txtAnswer.AppendText("Error: " & ex.Message)
+        End Try
+
+    End Sub
+
+    Sub SpeechToText(ByVal s As String)
+
+        If chkMute.Checked Then
+            Exit Sub
+        End If
+
+        If oSpeechSynthesizer Is Nothing Then
+            oSpeechSynthesizer = New System.Speech.Synthesis.SpeechSynthesizer()
+            oSpeechSynthesizer.SetOutputToDefaultAudioDevice()
+        End If
+
+        If cbVoice.Text <> "" Then
+            oSpeechSynthesizer.SelectVoice(cbVoice.Text)
+        End If
+
+        oSpeechSynthesizer.Speak(s)
+
+    End Sub
+
+    Function SendMsg(ByVal sQuestion As String)
+
+        System.Net.ServicePointManager.SecurityProtocol =
+           System.Net.SecurityProtocolType.Ssl3 Or
+           System.Net.SecurityProtocolType.Tls12 Or
+           System.Net.SecurityProtocolType.Tls11 Or
+           System.Net.SecurityProtocolType.Tls
+
+        Dim sModel As String = cbModel.Text 'text-davinci-002, text-davinci-003
+        Dim sUrl As String = "https://api.openai.com/v1/completions"
+
+        If sModel.IndexOf("gpt-3.5-turbo") <> -1 Then
+            'Chat GTP 4 https://openai.com/research/gpt-4
+            sUrl = "https://api.openai.com/v1/chat/completions"
+        End If
+
+        Dim request As HttpWebRequest = WebRequest.Create(sUrl)
+        request.Method = "POST"
+        request.ContentType = "application/json"
+        request.Headers.Add("Authorization", "Bearer " & OPENAI_API_KEY)
+
+        Dim iMaxTokens As Integer = txtMaxTokens.Text '2048
+
+        Dim dTemperature As Double = txtTemperature.Text '0.5
+        If dTemperature < 0 Or dTemperature > 1 Then
+            MsgBox("Randomness has to be between 0 and 1 with higher values resulting in more random text")
+            Return ""
+        End If
+
+        Dim sUserId As String = txtUserID.Text '1
+
+        'https://beta.openai.com/docs/api-reference/completions/create
+        Dim data As String = ""
+
+        If sModel.IndexOf("gpt-3.5-turbo") <> -1 Then
+            'Chat GTP 4
+            data = "{"
+            data += " ""model"":""" & sModel & ""","
+            data += " ""messages"": [{""role"":""user"", ""content"": """ & PadQuotes(sQuestion) & """}]"
+            data += "}"
+        Else
+            data = "{"
+            data += " ""model"":""" & sModel & ""","
+            data += " ""prompt"": """ & PadQuotes(sQuestion) & ""","
+            data += " ""max_tokens"": " & iMaxTokens & ","
+            data += " ""user"": """ & sUserId & """, "
+            data += " ""temperature"": " & dTemperature & ", "
+            data += " ""frequency_penalty"": 0.0" & ", " 'Number between -2.0 and 2.0  Positive value decrease the model's likelihood to repeat the same line verbatim.
+            data += " ""presence_penalty"": 0.0" & ", " ' Number between -2.0 and 2.0. Positive values increase the model's likelihood to talk about new topics.
+            data += " ""stop"": [""#"", "";""]" 'Up to 4 sequences where the API will stop generating further tokens. The returned text will not contain the stop sequence.
+            data += "}"
+        End If
+
+        Using streamWriter As New StreamWriter(request.GetRequestStream())
+            streamWriter.Write(data)
+            streamWriter.Flush()
+            streamWriter.Close()
+        End Using
+
+        Dim response As HttpWebResponse = request.GetResponse()
+        Dim streamReader As New StreamReader(response.GetResponseStream())
+        Dim sJson As String = streamReader.ReadToEnd()
+        'Return sJson
+
+        Dim oJavaScriptSerializer As New System.Web.Script.Serialization.JavaScriptSerializer
+        Dim oJson As Hashtable = oJavaScriptSerializer.Deserialize(Of Hashtable)(sJson)
+        Dim sResponse As String = ""
+
+        If sModel.IndexOf("gpt-3.5-turbo") <> -1 Then
+            'Chat GTP 4
+            sResponse = oJson("choices")(0)("message")("content")
+        Else
+            sResponse = oJson("choices")(0)("text")
+        End If
+
+        Return sResponse
+    End Function
+
+    Private Sub SetModels()
+        'https://beta.openai.com/docs/models/gpt-3
+
+        System.Net.ServicePointManager.SecurityProtocol =
+           System.Net.SecurityProtocolType.Ssl3 Or
+           System.Net.SecurityProtocolType.Tls12 Or
+           System.Net.SecurityProtocolType.Tls11 Or
+           System.Net.SecurityProtocolType.Tls
+
+        Dim apiEndpoint As String = "https://api.openai.com/v1/models"
+        Dim request As HttpWebRequest = WebRequest.Create(apiEndpoint)
+        request.Method = "GET"
+        request.ContentType = "application/json"
+        request.Headers.Add("Authorization", "Bearer " & OPENAI_API_KEY)
+
+        Dim response As HttpWebResponse = request.GetResponse()
+        Dim streamReader As New StreamReader(response.GetResponseStream())
+        Dim sJson As String = streamReader.ReadToEnd()
+        'Return sJson
+
+        cbModel.Items.Clear()
+
+        'Dim sIds As String = ""
+        Dim oSortedList As SortedList = New SortedList()
+        Dim oJavaScriptSerializer As New System.Web.Script.Serialization.JavaScriptSerializer
+        Dim oJson As Hashtable = oJavaScriptSerializer.Deserialize(Of Hashtable)(sJson)
+        Dim oList As Object() = oJson("data")
+        For i As Integer = 0 To oList.Length - 1
+            Dim sId As String = oList(i)("id")
+            'Dim sCreated As String = oList(i)("created")
+            'Dim sObject As String = oList(i)("object")
+            'Dim sOwned_by As String = oList(i)("owned_by")
+            'Dim oPermission As Hashtable = oList(i)("permission")(0)
+            'If sIds <> "" Then sIds += vbCrLf
+            'sIds += sCreated & vbTab & sId
+            oSortedList.Add(sId, sId)
+        Next
+
+        For Each oItem As DictionaryEntry In oSortedList
+            cbModel.Items.Add(oItem.Key)
+        Next
+
+
+    End Sub
+
+    Private Function PadQuotes(ByVal s As String) As String
+
+        If s.IndexOf("\") <> -1 Then
+            s = Replace(s, "\", "\\")
+        End If
+
+        If s.IndexOf(vbCrLf) <> -1 Then
+            s = Replace(s, vbCrLf, "\n")
+        End If
+
+        If s.IndexOf(vbCr) <> -1 Then
+            s = Replace(s, vbCr, "\r")
+        End If
+
+        If s.IndexOf(vbLf) <> -1 Then
+            s = Replace(s, vbLf, "\f")
+        End If
+
+        If s.IndexOf(vbTab) <> -1 Then
+            s = Replace(s, vbTab, "\t")
+        End If
+
+        If s.IndexOf("""") = -1 Then
+            Return s
+        Else
+            Return Replace(s, """", "\""")
+        End If
+    End Function
+
+End Class
